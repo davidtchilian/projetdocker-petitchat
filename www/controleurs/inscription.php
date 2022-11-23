abd8f7f24d94463411bc14f6c38867c249b249db
David TCHILIAN <davidtchilian@hotmail.com>
2022-11-23 21:19:30 +0100

    &1

--- www/controleurs/inscription.php
+++ www/controleurs/inscription.php
@@ -1,11 +1,5 @@
 <?php 
-	error_reporting(E_ALL);
-	ini_set('display_errors', 1);
-
 	require_once("../modele/bd.php");
-	$bd=new Bd();
-	$co = $bd->connexion();
-
 
 	$pseudo = htmlspecialchars(addslashes($_POST['pseudo']));
 	$mdp = htmlspecialchars(addslashes($_POST['mdp']));
@@ -14,7 +8,7 @@
 
 	
 	$sql = "INSERT INTO membres(membres_pseudo, membres_mdp) VALUES ('$pseudo', '$mdp')";
-	// echo $sql;
-	$result  = mysqli_query($co, $sql);
+	$result  = pg_query($co, $sql);
 	header('Location: ../index.php?err=4');
+	exit(1);
 ?>
