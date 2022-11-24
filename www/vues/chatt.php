b88c2cd5203962ec5f15b87d03668b4f0fb915b1
David TCHILIAN <davidtchilian@hotmail.com>
2022-11-24 01:56:20 +0100

    &1

--- www/vues/chatt.php
+++ www/vues/chatt.php
@@ -99,9 +99,6 @@ if (count($row) > 0) {
 </body>
 <script>
 	var conn = new WebSocket('ws://localhost:8080');
-	// conn.onopen = function(e) {
-	// 	console.log("Connection established!");
-	// };
 
 	conn.onmessage = function(e) {
 		console.log(e.data);
